e549568327c0478c66f94793a6af9fbf684274f6
可超 陈 <648929783@qq.com>
2019-07-21 10:51:36 +0800

    start

---
+++ register-server/src/main/java/com/open/cloud/project/RegisterApplication.java
@@ -0,0 +1,18 @@
+package com.open.cloud.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chenkechao
+ * @date 2019-07-20 16:24
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterApplication.class, args);
+    }
+}
